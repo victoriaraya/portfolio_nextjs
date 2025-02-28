c94f296b38c55d2717041a87aa377f736e4746c7
Victoria Raya <victoriaraya111@gmail.com>
2025-02-27 16:28:45 -0800

    style edits for project pages

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,7 @@ import { ProjectList } from "@/components/ProjectsDoc";
 
 const Projects = () => {
   return (
-    <div className="min-h-screen w-screen mt-0 pt-4 sm:pt-12 pb-4 sm:pb-8 bg-[#b291a4] text-white grid sm:grid-cols-2 gap-7 sm:gap-9 px-2 sm:px-8 dark:bg-[#4a3f47] dark:text-gray-100">
+    <div className="min-h-screen w-screen mt-0 pt-4 sm:pt-12 pb-4 sm:pb-8 bg-[#b291a4] text-white grid sm:grid-cols-2 gap-y-10 gap-x-5 sm:gap-y-20 sm:gap-x-9 lg:gap-x-2 px-2 sm:px-8 dark:bg-[#4a3f47] dark:text-gray-100 mb-5 sm:mb-3 justify-center">
       {ProjectList.map((project, idx) => (
         <ProjectTile project={project} key={idx} />
       ))}
--- components/ProjectTile.tsx
+++ components/ProjectTile.tsx
@@ -7,39 +7,41 @@ const ProjectTile = (props) => {
   return (
     <div className="relative text-white group dark:text-gray-100">
       <Link href={`/projects/${project.urlName}`}>
-        {project.name.includes("Email") ? (
-          <Image
-            src={`/images/${project.images[0]}.png`}
-            alt={`${project.name}`}
-            width={550}
-            height={400}
-            quality={100}
-            className="transition-all duration-200 ease-in-out transform group-hover:scale-95 rounded-sm group-hover:bg-black group-hover:opacity-60 dark:group-hover:bg-gray-800 dark:grayscale dark:brightness-75"
-          />
-        ) : (
-          <video
-            autoPlay
-            muted
-            loop
-            playsInline
-            className={
-              project.name.includes("Meaningful")
-                ? "transition-all duration-200 ease-in-out transform group-hover:scale-95 rounded-sm group-hover:bg-black group-hover:opacity-60 dark:group-hover:bg-gray-800 dark:grayscale dark:brightness-75"
-                : "transition-all duration-200 ease-in-out transform group-hover:scale-95 rounded-sm group-hover:bg-black group-hover:opacity-60 dark:group-hover:bg-gray-800 dark:grayscale dark:brightness-75"
-            }
-          >
-            <source
-              src={`/images/${project.imageName}.mp4`}
-              type="video/mp4"
-              loading="lazy"
+        <div className="flex justify-center w-full">
+          {project.name.includes("Email") ? (
+            <Image
+              src={`/images/${project.images[0]}.png`}
+              alt={`${project.name}`}
+              width={550}
+              height={400}
+              quality={100}
+              className="transition-all duration-200 ease-in-out transform sm:group-hover:scale-95 rounded-sm sm:group-hover:bg-black sm:group-hover:opacity-60 dark:group-hover:bg-gray-800 dark:grayscale dark:brightness-75 sm:w-full lg:w-[95%] sm:h-[300px] lg:h-[350px] sm:object-cover lg:object-fill object-center"
             />
-          </video>
-        )}
-        <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 rounded-sm">
+          ) : (
+            <video
+              autoPlay
+              muted
+              loop
+              playsInline
+              className={
+                project.name.includes("Meaningful")
+                  ? "transition-all duration-200 ease-in-out transform sm:group-hover:scale-95 rounded-sm sm:group-hover:bg-black sm:group-hover:opacity-60 dark:group-hover:bg-gray-800 dark:grayscale dark:brightness-75 sm:w-full lg:w-[95%] sm:h-[300px] lg:h-[350px] sm:object-cover lg:object-fill object-center"
+                  : "transition-all duration-200 ease-in-out transform sm:group-hover:scale-95 rounded-sm sm:group-hover:bg-black sm:group-hover:opacity-60 dark:group-hover:bg-gray-800 dark:grayscale dark:brightness-75 sm:w-full lg:w-[95%] sm:h-[300px] lg:h-[350px] sm:object-cover lg:object-fill object-center"
+              }
+            >
+              <source
+                src={`/images/${project.imageName}.mp4`}
+                type="video/mp4"
+                loading="lazy"
+              />
+            </video>
+          )}
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 sm:group-hover:opacity-100 rounded-sm">
           <p className="text-2xl font-black dark:text-gray-200">See project</p>
         </div>
       </Link>
-      <p className="text-2xl mt-1 sm:mt-2">
+      <p className="text-2xl mt-1 sm:mt-2 text-center">
         {project.name} - {project.category}
       </p>
     </div>
